ebf486d3851b46125d2798c3ec1444a65b61f772
fernandacardial <fernandacardial@outlook.com>
2025-05-20 12:51:05 -0300

    Implantação redefinição de senha

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { login } from "../controllers/authController";
+import {  login,  cadastro,  recuperarSenha,  redefinirSenha} from "../controllers/authController";
 
 const authRoutes = Router();
-authRoutes.post('/login',login);
+
+authRoutes.post('/login', login);
+authRoutes.post('/cadastro', cadastro);
+authRoutes.post('/recuperar-senha', recuperarSenha);
+authRoutes.put('/redefinir-senha', redefinirSenha);
+
 export default authRoutes;
