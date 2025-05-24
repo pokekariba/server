96511e40a3507a028713f486c3ae13930644fb41
Matheus Braz de Oliveira <theus.braz18@gmail.com>
2025-05-24 00:14:47 -0300

    hotfix: cors

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,17 @@ import protegidas from "./routes/protegidas";
 import { autenticador } from "./middleware/autenticador.middleware";
 import authRoutes from "./routes/authRoutes";
 const app = express();
-app.use(cors({
-  origin: 'http://localhost:8080',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200); // responde o preflight diretamente
+  }
+
+  next();
+});
 app.use(express.json());
 app.use(helloRoutes);
 app.use('/jogo',autenticador,protegidas);
