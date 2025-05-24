c22e87d8db9ba15b8989d7dff4fe539e5c8bf2e0
fernandacardial <fernandacardial@outlook.com>
2025-05-24 00:22:45 -0300

    MFLP-23: Adicionar item

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import cors from "cors";
 import protegidas from "./routes/protegidas";
 import { autenticador } from "./middleware/autenticador.middleware";
 import authRoutes from "./routes/authRoutes";
+import backofficeRoutes from "./routes/backofficeRoutes";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(helloRoutes);
 app.use('/jogo',autenticador,protegidas);
 app.use(authRoutes);
+app.use("/backoffice",autenticador,backofficeRoutes);
 export default app;
--- src/routes/backofficeRoutes.ts
+++ src/routes/backofficeRoutes.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
+import { adicionarItem } from "../controllers/itemController";
+import { autenticador } from "../middleware/autenticador.middleware";  // importa o middleware
 
 const router = Router();
-router.get('/exemplo',(req,resp) => {
-    resp.send('Você entrou no Backoffice');
+
+router.get('/exemplo', (req, resp) => {
+  resp.send('Você entrou no Backoffice');
 });
+
+// Rota protegida: o middleware autenticador vai executar antes do controlador
+router.post('/adicionar-item', autenticador, adicionarItem);
+
 export default router;
