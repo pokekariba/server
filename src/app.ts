3c6c0e007a1a31817c3e073a0fcfb3465ebf9d57
matheusboliveir <theus.braz18@gmail.com>
2025-05-26 18:34:24 -0300

    hotfix: retirando imports do app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
-import helloRoutes from "./routes/hello";
-import protegidasRoutes from "./routes/protegidas"; // protegidas
 import express from "express";
 import protegidas from "./routes/protegidas";
 import { autenticador } from "./middleware/autenticador.middleware";
 import authRoutes from "./routes/authRoutes";
-import imagemItemRoutes from "./routes/backoffice/imagemItem";
 
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-  if (req.method === 'OPTIONS') {
+  if (req.method === "OPTIONS") {
     res.sendStatus(200);
     return;
   }
@@ -20,9 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
-app.use('/jogo',autenticador,protegidas);
+app.use("/jogo", autenticador, protegidas);
 app.use(authRoutes);
 
 export default app;
-
-
