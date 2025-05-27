e3608d9185d40a7040f9b2c1bee39b81d336f7d0
Gabriel Nieri <gabrielnieri43@gmail.com>
2025-05-27 14:12:22 -0300

    MFLP 21 - Visualizar Imagem

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { uploadParaCloudinary } from "../config/cloudinary.config";
 import { PrismaClient } from "@prisma/client";
 import { gerarNomeImagem, sanitizarImagem } from "../utils/imageUtil";
+import cloudinary from "../config/cloudinary.config";
 
 const prisma = new PrismaClient();
 
@@ -37,5 +38,29 @@ export const adicionarImagemItem = async (req: Request, res: Response) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Erro ao processar imagens" });
+  }
+};
+
+export const visualizarItem = async (req: Request, res: Response): Promise<void> => {
+  const { itemId } = req.body;
+
+  if (typeof itemId !== "number") {
+    res.status(400).json({ mensagem: "itemId inválido" });
+    return;
+  }
+
+  try {
+    const resultado = await cloudinary.search
+      .expression(`public_id:itens/${itemId}/item_${itemId}*`)
+      .sort_by("created_at", "desc")
+      .max_results(30)
+      .execute();
+
+    const urlsItem = resultado.resources.map((img: any) => img.secure_url);
+
+    res.status(200).json({ urlsItem });
+  } catch (error) {
+    console.error("Erro ao buscar imagens no Cloudinary:", error);
+    res.status(500).json({ mensagem: "Erro interno tente novamente mais tarde." });
   }
 };
--- src/routes/lojaRoutes.ts
+++ src/routes/lojaRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
+import { visualizarItem } from "../controllers/imageController";
 import { moedas } from "../controllers/moedasController";
 import { comprarItem } from "../controllers/comprarController";
 
 const lojaRoutes = Router();
 
 lojaRoutes.post("/comprar-item", comprarItem);
-
 lojaRoutes.post("/comprar-moedas", moedas);
+lojaRoutes.post("/visualizar-item", visualizarItem);
 
 export default lojaRoutes;
