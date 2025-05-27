26602f24baf01c61fdc5e3f073864e0389d9d925
matheusboliveir <theus.braz18@gmail.com>
2025-05-27 00:42:59 -0300

    MFLP-26: ajuste nas pastas

--- src/routes/backoffice/usuario.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { listarUsuariosController } from "../../controllers/backoffice/listarUsuarios";
-
-const router = Router();
-
-router.post("/backoffice/usuarios", listarUsuariosController);
-
-export default router;
