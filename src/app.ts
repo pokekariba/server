fd887b07f1898e211b5c4a9b60ae60688491b275
matheusboliveir <theus.braz18@gmail.com>
2025-06-15 18:49:03 -0300

    .

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,10 @@ import express from "express";
 import protegidas from "./routes/protegidas";
 import { autenticador } from "./middleware/autenticador.middleware";
 import authRoutes from "./routes/authRoutes";
-import { env } from "./config/env";
 
 const app = express();
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (origin && env.allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
